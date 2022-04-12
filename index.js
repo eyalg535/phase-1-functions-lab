a4dcedda1035d193816f6534878f8de520117530
eyal gerby <eyalg535@gmail.com>
2022-04-12 13:54:31 -0400

    commit tree 496cd0299bd44c2fd5f846bee516b24f037a9aa0

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+
+
+const HQ = 42;
+const FEET = 264;
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - HQ); 
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * FEET;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * FEET;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start,destination);
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000){
+        return ((distanceInFeet - 400) * 2) / 100;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return calculatesFarePrice = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+  }
+
